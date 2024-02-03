d108b3ce4fc5b5fa436523ac0e35ad1e4dd3e353
seokgeun <gkwoap@gmail.com>
2024-02-03 11:18:55 +0900

    practice - spring data jpa,aop

--- spring-study/spring-introduction/practice/intro/src/main/java/com/practice/intro/config/SpringConfig.java
+++ spring-study/spring-introduction/practice/intro/src/main/java/com/practice/intro/config/SpringConfig.java
@@ -2,7 +2,6 @@ package com.practice.intro.config;
 
 import com.practice.intro.repository.*;
 import com.practice.intro.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,23 +21,35 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }
      */
-    private EntityManager em;
+    /**
+     * JPA - EntityManager 주입
+     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+*/
+    // Spring Data Jpa 주입
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
+    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }

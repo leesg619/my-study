c7db870b09b8936548e4326c9470a22bc8c77160
seokgeun <gkwoap@gmail.com>
2024-02-15 23:02:45 +0900

    init practice (ptg)

--- test-code-study/practical-test-guide/practice/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ test-code-study/practical-test-guide/practice/cafekiosk/src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,4 +1,50 @@
 package sample.cafekiosk.unit;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.beverage.order.Order;
+
+@Getter
 public class CafeKiosk {
+
+	private final List<Beverage> beverages = new ArrayList<>();
+
+	public void add(Beverage beverage) {
+		beverages.add(beverage);
+	}
+
+	public void add(Beverage beverage, int count) {
+		if (count <= 0) {
+			throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
+		}
+
+		for (int i = 0; i < count; i++) {
+			beverages.add(beverage);
+		}
+	}
+
+	public void remove(Beverage beverage) {
+		beverages.remove(beverage);
+	}
+
+	public void clear() {
+		beverages.clear();
+	}
+
+	public int calculateTotalPrice() {
+		int totalPrice = 0;
+		for (Beverage beverage : beverages) {
+			totalPrice += beverage.getPrice();
+		}
+		return totalPrice;
+	}
+
+	public Order createOrder() {
+		return new Order(LocalDateTime.now(), beverages);
+	}
+
 }
